87bdb448eb366f764072eb4fb3739d7939772fb9
BenaJovi <xavibenavides@yahoo.com>
2023-02-28 23:12:41 -0800

    Add project files.

---
+++ COMP003A.Assignment7/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace COMP003A.Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SectionSeparator("Array - CharacterCounter Section");
+
+
+        }
+
+
+
+
+        /// <summary>
+        /// Section Seperator 
+        /// </summary>
+        /// <param name="section"></param>
+        static void SectionSeparator(string section)
+        {
+            Console.WriteLine("".PadRight(50, '*') + $"\n\t{section} Section\n" + "".
+        PadRight(50, '*'));
+        }
+
+       
+        static void CharacterCounter(char CharacterInput, string word)
+        {
+            
+            
+        }
+
+
+    }
+}
